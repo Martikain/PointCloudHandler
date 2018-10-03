cf2c459c48a3c78e728bf348345f58a1ce8f87a7
Ossi Martikainen <ossi.martikainen@gmail.com>
2018-10-03 19:12:07 +0300

    Changed PCDWriter approach to using PCL for enabling binary writing.

--- pcdwriter.cpp
+++ pcdwriter.cpp
@@ -1,32 +1,34 @@
 #include "pcdwriter.h"
 
-PCDWriter::PCDWriter()
+
+PCDWriter::PCDWriter(const bool &safeWrite) : addFalsePt_(!safeWrite)
 {
 }
+
 
 void PCDWriter::storeIntensity(const bool &val)
 {
-    storeIntensity_ = val;
+    writeIntensity_ = val;
 }
 
 
-void PCDWriter::addPoint(const float &x, const float &y,
-                                  const float &z)
+void PCDWriter::addPoint(const float &x, const float &y, const float &z)
 {
-    PointData newPoint{x, y, z, 0.0};
-    currentPoints_.push_back(newPoint);
+    if ( writeIntensity_ )
+    {
+        qDebug() << "ERROR: Trying to add XYZ point to cloud requesting intensity";
+        if ( addFalsePt_ )
+            addPoint(x,y,z,DEFAULT_INTENSITY);
+    }
+
+    pcl::PointXYZ p(x, y, z);
+    xyzCloud_.push_back(p);
 }
 
 
-void PCDWriter::addPoint(const float &x, const float &y,
-                                  const float &z, const float &intensity)
+void PCDWriter::addPoint(const float &x, const float &y, const float &z, const float &intensity)
 {
-    if ( !storeIntensity_ )
-        qDebug() << "WARNING: Intensity value is not stored.";
 
-    addPoint(x, y, z);
-    PointData intensityVal{intensity, 0.0, 0.0, 0.0};
-    currentPoints_.push_back(intensityVal);
 }
 
 
@@ -39,40 +41,4 @@ void PCDWriter::writeBinary(const QString &filePath)
 void PCDWriter::writeAscii(const QString &filePath)
 {
 
-}
-
-
-QString PCDWriter::createPcdHeader(const bool &binary)
-{
-    QString header = "# .PCD v.7 - Point Cloud Data file format\n";
-    header += "VERSION .7\n";
-    QString numOfPoints;
-
-    if ( storeIntensity_ )
-    {
-        numOfPoints = (QString::number(currentPoints_.size()/2));
-        header += "FIELDS x y z intensity\n";
-        header += "SIZE 4 4 4 4\n";
-        header += "TYPE 4 4 4 4\n";
-        header += "COUNT 1 1 1 1\n";
-    } else
-    {
-        numOfPoints = (QString::number(currentPoints_.size()));
-        header += "FIELDS x y z\n";
-        header += "SIZE 4 4 4\n";
-        header += "TYPE 4 4 4\n";
-        header += "COUNT 1 1 1\n";
-    }
-
-    header += "WIDTH " + numOfPoints + "\n";
-    header += "HEIGHT 1\n";
-    header += "WIEWPOINT 0 0 0 1 0 0 0\n";
-    header += "POINTS " + numOfPoints + "\n";
-
-    if ( binary )
-        header += "DATA binary\n";
-    else
-        header += "DATA ascii\n";
-
-    return header;
 }
--- pcdwriter.h
+++ pcdwriter.h
@@ -1,42 +1,49 @@
 #ifndef PCDBINARYWRITER_H
 #define PCDBINARYWRITER_H
 
-#include <list>
+#define DEFAULT_INTENSITY 100.0
+
 #include <QDebug>
-#include <QDataStream>
-#include <QString>
-#include <QFile>
-#include <QDir>
-
-struct PointData
-{
-    float xOrIntensity;
-    float y;
-    float z;
-    float reserved;
-};
-
+#include <pcl/point_types.h>
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
 
 class PCDWriter
 {
 public:
-    PCDWriter();
 
+    // Setting safeWrite to false enables the use of
+    // addPoint() function with intensity even if it
+    // is not stored or vice versa. If true, calling
+    // addPoint() in wrong manner will only cause error
+    // texts and no point is added.
+    PCDWriter(const bool &safeWrite = true);
 
+    // Defines whether intensity values are stored in the PCD file
+    // If true, all future addPoint() calls must have intensity values
+    // If false, all future addPoint() calls must omit intensity
     void storeIntensity(const bool &val);
+
+    // Functions for adding points to the point cloud
     void addPoint(const float &x, const float &y, const float &z);
     void addPoint(const float &x, const float &y,
                      const float &z, const float &intensity);
 
+    // Creates a binary format PCD file
     void writeBinary(const QString &filePath);
+
+    // Creates an ASCII format PCD file
     void writeAscii(const QString &filePath);
 
 private:
 
-    bool storeIntensity_;
-    std::list<PointData> currentPoints_;
+    bool writeIntensity_;
+    bool addFalsePt_; // If true, will add points to cloud even if
+                      // intensity is required and it is not provided
+                      // or vice versa
 
-    QString createPcdHeader(const bool &binary);
+    pcl::PointCloud<pcl::PointXYZ> xyzCloud_;
+    pcl::PointCloud<pcl::PointXYZI> xyziCloud_;
 };
 
 #endif // PCDBINARYWRITER_H
