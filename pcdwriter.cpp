e49ca10264cda25e747e80df9e1c46344177c1fa
Ossi Martikainen <ossi.martikainen@gmail.com>
2018-10-03 19:23:22 +0300

    Added addPoint() functions.

--- pcdwriter.cpp
+++ pcdwriter.cpp
@@ -16,9 +16,11 @@ void PCDWriter::addPoint(const float &x, const float &y, const float &z)
 {
     if ( writeIntensity_ )
     {
-        qDebug() << "ERROR: Trying to add XYZ point to cloud requesting intensity";
+        qDebug() << "ERROR: Trying to add XYZ point "
+                    "to cloud requesting intensity.";
         if ( addFalsePt_ )
             addPoint(x,y,z,DEFAULT_INTENSITY);
+        return;
     }
 
     pcl::PointXYZ p(x, y, z);
@@ -26,15 +28,29 @@ void PCDWriter::addPoint(const float &x, const float &y, const float &z)
 }
 
 
-void PCDWriter::addPoint(const float &x, const float &y, const float &z, const float &intensity)
+void PCDWriter::addPoint(const float &x, const float &y,
+                         const float &z, const float &intensity)
 {
+    if ( !writeIntensity_ )
+    {
+        qDebug() << "ERROR: Trying to add XYZI point "
+                    "to cloud without intensity values.";
+        if ( addFalsePt_ )
+            addPoint(x,y,z);
+        return;
+    }
 
+    pcl::PointXYZI p(intensity);
+    p.x = x;
+    p.y = y;
+    p.z = z;
+    xyziCloud_.push_back(p);
 }
 
 
 void PCDWriter::writeBinary(const QString &filePath)
 {
-
+    int i = 0;
 }
 
 
